cd194897904045804b7edb043fd1ce7d42d4c271
Vivek Katial <vivekkatial@gmail.com>
2021-06-10 15:49:17 +1000

    Adding github 2021 presso

---
+++ 2021/matilda-git-workshop/examples/merge_conflict.py
@@ -0,0 +1,10 @@
+'''
+Example script for showing how to fix merge-conflicts
+
+Author: Vivek Katial
+'''
+
+last_name = "Katial"
+first_name = "Vivek"
+title = "Mr"
+print(title +  first_name + last_name)
